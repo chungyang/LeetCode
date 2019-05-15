fdd2c7468926b623b059ff0afbfa004160dc3b27
chung <chung.yang@engineer.uconn.edu>
2019-05-15 03:43:46 -0400

    more concise solution

--- src/MaxIncreasetoKeepCitySkyline.java
+++ src/MaxIncreasetoKeepCitySkyline.java
@@ -4,24 +4,16 @@ class Solution {
         int[] rowMaxNumbers = new int[grid.length];
         int[] colMaxNumbers = new int[grid[0].length];
         
-        int total = 0;
         for(int i = 0; i < grid.length; i++){
             
-            int rowMaxNumber = 0;
             for(int j = 0; j < grid[0].length; j++){
-                
-                if(grid[i][j] > rowMaxNumber){
-                    rowMaxNumber = grid[i][j];
-                }
-                
-                if(grid[i][j] > colMaxNumbers[j]){
-                    colMaxNumbers[j] = grid[i][j];
-                }
+                rowMaxNumbers[i] = Math.max(rowMaxNumbers[i], grid[i][j]);
+                colMaxNumbers[j] = Math.max(colMaxNumbers[j], grid[i][j]);
             }
             
-            rowMaxNumbers[i] = rowMaxNumber;
         }
         
+        int total = 0;
         for(int i = 0; i < rowMaxNumbers.length; i++){
             for(int j = 0; j < colMaxNumbers.length; j++){
                 total += Math.min(rowMaxNumbers[i], colMaxNumbers[j]) - grid[i][j];
